2dd70c43a1f42a4412a29e8b4161d457a802cdd3
Eugeny Leonov <eleonov@leolab.info>
2021-04-17 15:52:46 +0500

    Init step two

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang:alpine
 WORKDIR /src
 COPY ./src .
+COPY ./static /static
 RUN go build -o /bin/sswc .
 ENTRYPOINT [ "/bin/sswc" ]
 EXPOSE 8080
--- src/main.go
+++ src/main.go
@@ -6,12 +6,28 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi there! Updated!")
+/*
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := os.Stat("/store/static/"); err == nil {
+	} else {
+	}
 }
 
+func resourceHandler(w http.ResponseWriter, r *http.Request) {
+
+	if _, err := os.Stat("/store/res/"); err == nil {
+	} else {
+	}
+}
+*/
+
 func main() {
-	http.HandleFunc("/", handler)
+	/*
+		http.HandleFunc("/", defaultHandler)
+		http.HandleFunc("/res/", resourceHandler)
+	*/
+	fs := http.FileServer(http.Dir("/static"))
+	http.Handle("/", fs)
 	fmt.Println("Starting server at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
